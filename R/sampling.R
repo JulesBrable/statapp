5d7e7e9c16ad3b0b6677274e3a5aafbaa73f54bc
JulesBrable <jules.brable7@gmail.com>
2023-01-18 12:10:10 +0100

    improve data extraction

--- R/sampling.R
+++ R/sampling.R
@@ -1,84 +1,13 @@
-
-# SETTING ENVIRONEMENT ----------------------------------------------------
-rm(list = ls())
-
-library(data.table)
-library(tidyverse)
-
-source(here::here("R/statdesc_functions.R"))
 source(here::here("R/sampling_functions.R"))
 
-list.files(path = "data")
-
-# issue with the column names #
-ndiff1 <- setdiff(
-  (sample_df("nat2019us.csv", n = 1, segment_treat = F) %>% colnames()),
-  (sample_df("nat2021us.csv", n = 1, segment_treat = F) %>% colnames())
-)
-
-ndiff2 <- setdiff(
-  (sample_df("nat2021us.csv", n = 1, segment_treat = F) %>% colnames()),
-  (sample_df("nat2019us.csv", n = 1, segment_treat = F) %>% colnames())
-)
-
-ndiff3 <- setdiff(
-  (sample_df("nat2021us.csv", n = 1, segment_treat = F) %>% colnames()),
-  (sample_df("nat2018us.csv", n = 1, segment_treat = F) %>% colnames())
-)
-
-to_remove <- c(ndiff1, ndiff2, ndiff3)
-
-
 ## import data ##
-# loading all 2021 data in order to compare
-tab2021 <- load_data("nat2021us.csv")
+# designing our sample #
 
-# designing our sample
+to_remove <- diff_2_csv("nat2019us.csv", "nat2021us.csv")
 n <- 300000
-s2021 <- sample_df("nat2021us.csv", n, remove = to_remove)
-s2020 <- sample_df("nat2020us.csv", n, remove = to_remove)
-s2019 <- sample_df("nat2019us.csv", n, remove = to_remove)
-s2018 <- sample_df("nat2018us.csv", n, remove = to_remove)
 
-mixsample <- s2021 %>% 
-  bind_rows(s2020) %>% 
-  bind_rows(s2019) %>% 
-  bind_rows(s2018)
+mixsample <- make_sample(n = n, to_remove = to_remove)
 
-# STAT DESC ---------------------------------------------------------------
+write_csv(mixsample, here::here("data", "data.csv"))
 
-tab2021 %>% 
-  get_prop(rf_inftr) %>% 
-  bind_rows(tab2021 %>% 
-              get_prop(rf_fedrg)) %>% 
-  bind_rows(tab2021 %>% 
-              get_prop(rf_artec)) %>% 
-  mutate(var = c("rf_inftr", "rf_fedrg", "rf_artec")) %>%
-  relocate(var) %>% 
-  kableExtra::kbl() %>% 
-  template()
-
-mixsample %>% 
-  get_prop(rf_inftr) %>% 
-  bind_rows(tab2021 %>% 
-              get_prop(rf_fedrg)) %>% 
-  bind_rows(tab2021 %>% 
-              get_prop(rf_artec)) %>% 
-  mutate(var = c("rf_inftr", "rf_fedrg", "rf_artec")) %>%
-  relocate(var) %>% 
-  kableExtra::kbl() %>% 
-  template()
-
-s2020 %>%
-  transform_age() %>% 
-  pyrage()
-
-tab2021 %>%
-  transform_age() %>% 
-  pyrage()
-
-tab2021 %>%
-  get_prop(rf_artec) %>%
-  kableExtra::kbl() %>% 
-  template()
-
+rm(list = ls())
--- R/sampling_functions.R
+++ R/sampling_functions.R
@@ -1,3 +1,13 @@
+library(tidyverse)
+
+# ---------------------------------------------------------------------
+
+load_data <- function(file, path = "data"){
+  
+  tab2021 <- arrow::read_csv_arrow(
+    here::here(path, file)
+  )
+}
 
 # -------------------------------------------------------------------------
 
@@ -45,4 +55,43 @@ sample_df <- function(file, n, segment_treat = TRUE, path = "data",
 
 # -------------------------------------------------------------------------
 
+diff_2_csv <- function(csv1, csv2, n = 1, segment_treat = F){
+  
+  # Checks if there is differences in the column names of 2 given csv
+  # Uses the {sample_df} function
+  #If so, return a vector of string containing these names
+  #Takes about 30 seconds to run.
+  
+  ndiff1 <- setdiff(
+    (sample_df(csv1, n = n, segment_treat = segment_treat) %>% colnames()),
+    (sample_df(csv2, n = n, segment_treat = segment_treat) %>% colnames())
+  )
+  
+  ndiff2 <- setdiff(
+    (sample_df(csv2, n = n, segment_treat = segment_treat) %>% colnames()),
+    (sample_df(csv1, n = n, segment_treat = segment_treat) %>% colnames())
+  )
+  
+  return(c(ndiff1, ndiff2))
+}
 
+# -------------------------------------------------------------------------
+
+make_sample <- function(n, to_remove){
+  
+  # sample the 4 dataset from 2018, 2019, 2020 and 2021
+  s2021 <- sample_df("nat2021us.csv",  n, remove = to_remove)
+  s2020 <- sample_df("nat2020us.csv", n, remove = to_remove)
+  s2019 <- sample_df("nat2019us.csv", n, remove = to_remove)
+  s2018 <- sample_df("nat2018us.csv", n, remove = to_remove)
+  
+  # then bind the rows of each sample all together
+  mixsample <- s2021 %>% 
+    dplyr::bind_rows(s2020) %>% 
+    dplyr::bind_rows(s2019) %>% 
+    dplyr::bind_rows(s2018)
+  
+  rm(s2021, s2020, s2019, s2018)
+  
+  return(mixsample)
+}
