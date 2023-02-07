030e690e86181c9601831bd079cf492a609f1c1f
JulesBrable <jules.brable7@gmail.com>
2023-02-07 10:00:04 +0100

    show evolution of treated population over years

---
+++ R/evolution_over_years.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+
+source("R/functions/sampling_functions.R")
+source("R/functions/statdesc_functions.R")
+
+nb_years <- 4
+# create and empty dataframe
+res <- data.frame(
+  matrix(
+    nrow = nb_years,
+    ncol = 2,
+    dimnames = list(NULL, c("Year", "perc_T"))
+    )
+  )
+
+# from 2018 to 2021:
+# - load the dataset
+# - get the proportion of treated
+for (i in seq(18, 21)) {
+  
+  res[i-17, 1] <- glue::glue("20{i}")
+
+  res[i-17, 2] <- load_data(glue::glue("nat20{i}us.csv")) %>% 
+    group_by(rf_inftr) %>% 
+    count() %>% 
+    ungroup() %>% 
+    mutate(freq = n * 100 / sum(n)) %>% 
+    filter(rf_inftr == "Y") %>% 
+    pull(freq) %>%
+    round(2)
+  
+}
+
+# convert year to date
+res$Year <- lubridate::ymd(paste0(res$Year, "0101"))
+
+res
+
+# plot the result
+res %>% 
+  ggplot(aes(x = Year, y = perc_T)) +
+  geom_line(size = 0.75)+
+  geom_point() +
+  ylim(0, 3) +
+  labs(
+    y = "Percentage Treated",
+    title = "Changes in the proportion of people treated between 2018 and 2021"
+    ) +
+  theme_formatted()
+
+
+
+
+
+
+
+
