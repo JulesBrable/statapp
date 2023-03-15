d69402d9cbc517fc68cf8ee001eb32e489698233
romane-reynal <romane.reynal@ensae.fr>
2023-03-15 09:34:46 +0100

    premiers résultats de coeff sur le matching

--- R/matching.R
+++ R/matching.R
@@ -25,7 +25,7 @@ file <- "data.csv"
 
 
 to_keep <- c("dbwt", "mager", "meduc", "fagecomb", "feduc", "frace6", "mrace15",
-             "rf_fedrg", "priorlive", "dmar", "cig_rec", "mhisp_r", "fhispx",
+             "rf_fedrg", "rf_artec", "priorlive", "dmar", "cig_rec", "mhisp_r", "fhispx",
              "no_infec", "sex", "gestrec3", "dplural")
 
 df <- load_data(file)%>%
@@ -53,6 +53,9 @@ df <- df %>%
 
 # recoding rf_fedrg as follows :  X, U, N -> 0 & Y -> 1
 df$rf_fedrg <- ifelse(df$rf_fedrg == "Y", 1, 0)
+
+# recoding rf_artec as follows :  X, U, N -> 0 & Y -> 1
+df$rf_artec <- ifelse(df$rf_artec == "Y", 1, 0)
 
 #on supprime les valeurs unknown
 df <- df %>% 
@@ -89,21 +92,24 @@ matk2k <- cem(treatment= "rf_fedrg", data=df, k2k=TRUE, drop = "dbwt")
 matk2k
 
 #----------------------------- matching : cem avec MatchIt----------------------
-m <- matchit(rf_fedrg ~ mager + meduc + fagecomb + feduc + frace6 + mrace6 +
+m_fedrg <- matchit(rf_fedrg ~ mager + meduc + fagecomb + feduc + frace6 + mrace6 +
         priorlive + dmar + cig_rec + mhisp_r + fhispx + no_infec + sex_M + gestrec3,
         data = df,
         method = "cem",
         estimand = "ATT") 
 
 #takes a lot of time
-summary(m) 
+summary(m_fedrg) 
 
 #on plot les distributions
-plot(m, type = "density", interactive = FALSE)
+plot(m_fedrg, type = "density", interactive = FALSE)
 
 #on cherche à représenter toutes les variables
-mD <- match.data(m)
-summary(mD)
+md_fedrg_T <- match.data(m, group = "treated")
+summary(md_fedrg_T)
+
+md_fedrg_C <- match.data(m, group = "control")
+summary(md_fedrg_C)
 
 #puis on estime l'ATT
 fit1 <- lm(dbwt ~ rf_fedrg * (mager + meduc + fagecomb + feduc + frace6 + mrace6 +
@@ -139,6 +145,43 @@ fitk2k <- lm(dbwt ~ rf_fedrg * (mager + meduc + fagecomb + feduc + frace6 + mrac
 avg_comparisons(fitk2k, variables = "rf_fedrg", vcov = ~subclass,
                 newdata = subset(mD_k2k, rf_fedrg == 1))
 
+
+#----------------------matching EXACT: cem avec MatchIt-----------------------
+#avec un match exact (essai avec la distance par défaut = Mahalanobis)
+mex <- matchit(rf_fedrg ~ mager + meduc + fagecomb + feduc + frace6 + mrace6 +
+                  priorlive + dmar + cig_rec + mhisp_r + fhispx + no_infec + sex_M + gestrec3,
+                data = df,
+                method = "exact",
+                estimand = "ATT") 
+
+#takes a lot of time
+summary(mex)
+
+#on plot les distributions
+plot(mex, type = "densiy", interactive = FALSE)
+
+#on cherche à représenter toutes les variables
+mD_ex <- match.data(mex)
+summary(mD_ex)
+
+mD_ex_T <- match.data(mex, group = "treated")
+summary(mD_ex_T)
+#à la main, moyenne dbwt pour les traités = 3183
+
+mD_ex_C <- match.data(mex, group="control")
+summary(mD_ex_C)
+#à la main, moyenne dbwt pour les contrôles = 3312 
+
+#puis on estime l'ATT
+fitex <- lm(dbwt ~ rf_fedrg * (mager + meduc + fagecomb + feduc + frace6 + mrace6 +
+                                  priorlive + dmar + cig_rec + mhisp_r + fhispx + no_infec + sex_M 
+                                + gestrec3), data = mD_ex)
+
+avg_comparisons(fitex, variables = "rf_fedrg", vcov = ~subclass,
+                newdata = subset(mD_ex, rf_fedrg == 1))
+
+#En estimant, on a un coefficient de -77,8
+
 #----------------------matching JUMEAUX: cem avec MatchIt-----------------------
 #on filtre la présence de naissances plural
 dfJ <- df %>% filter(dplural != 1)
@@ -153,6 +196,29 @@ summary(m_J)
 
 #on plot les distributions
 plot(m_J, type = "density", interactive = FALSE)
+
+#on cherche à représenter toutes les variables
+mD_cem_J <- match.data(m_J)
+summary(mD_cem_J)
+
+mD_cem_J_T <- match.data(m_J, group = "treated")
+summary(mD_cem_J_T)
+#à la main, moyenne dbwt pour les traités = 2363
+
+mD_cem_J_C <- match.data(m_J, group="control")
+summary(mD_cem_J_C)
+#à la main, moyenne dbwt pour les contrôles = 2387 
+
+#puis on estime l'ATT
+fitcem_J <- lm(dbwt ~ rf_fedrg * (mager + meduc + fagecomb + feduc + frace6 + mrace6 +
+                                 priorlive + dmar + cig_rec + mhisp_r + fhispx + no_infec + sex_M 
+                               + gestrec3), data = mD_cem_J)
+
+avg_comparisons(fitcem_J, variables = "rf_fedrg", vcov = ~subclass,
+                newdata = subset(mD_cem_J, rf_fedrg == 1))
+
+#En estimant, on a un coefficient de -14,1
+
 
 
 
@@ -183,6 +249,28 @@ summary(m_S)
 
 #on plot les distributions
 plot(m_S, type = "density", interactive = FALSE)
+
+#on cherche à représenter toutes les variables
+mD_cem_S <- match.data(m_S)
+summary(mD_cem_S)
+
+mD_cem_S_T <- match.data(m_S, group = "treated")
+summary(mD_cem_S_T)
+#à la main, moyenne dbwt pour les traités = 3139
+
+mD_cem_S_C <- match.data(m_S, group="control")
+summary(mD_cem_S_C)
+#à la main, moyenne dbwt pour les contrôles = 3276 
+
+#puis on estime l'ATT
+fitcem_S <- lm(dbwt ~ rf_fedrg * (mager + meduc + fagecomb + feduc + frace6 + mrace6 +
+                                    priorlive + dmar + cig_rec + mhisp_r + fhispx + no_infec + sex_M 
+                                  + gestrec3), data = mD_cem_S)
+
+avg_comparisons(fitcem_S, variables = "rf_fedrg", vcov = ~subclass,
+                newdata = subset(mD_cem_S, rf_fedrg == 1))
+
+#En estimant, on a un coefficient de -78.5
 
 
 
