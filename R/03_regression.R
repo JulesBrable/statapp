0be199e0b9b00584e4e86a0ebd4a9933eb87092b
PB_ENSAE_PROJETS <ensaeprojets@MacBook-Pro-de-BIAN.local>
2023-04-26 23:13:12 +0200

    new linear regressions

--- R/03_regression.R
+++ R/03_regression.R
@@ -135,4 +135,34 @@ reg %>%
     out = glue::glue("reports/reg_{today}.html")
   )
 
+# Taking gest_under_37_weeks as interest variable but excluding dplural from the control variable
 
+df_reg_premature_drop_dplural <- readRDS(paste(path, "preprocessed_data.rds", sep = "/"))
+df_reg_premature_drop_dplural <- df_reg_premature_drop_dplural%>% select(!c(dplural,dbwt))
+
+reg_premature_drop_dplural <- lm(gest_under_37_weeks ~ .,
+                    data = df_reg_premature_drop_dplural)
+
+summary(reg_premature_drop_dplural)
+
+# Taking under_1500 as interest variable 
+
+df_reg_under_1500<- readRDS(paste(path, "preprocessed_data.rds", sep = "/"))
+df_reg_under_1500$dbwt_inf_1500 <- ifelse(df_reg_under_1500$dbwt < 1500 , 1, 0)
+df_reg_under_1500 <- df_reg_under_1500%>% select(!c(dplural,dbwt,gest_under_37_weeks))
+
+reg_under_1500 <- lm(dbwt_inf_1500 ~ .,
+                                 data = df_reg_under_1500)
+
+summary(reg_under_1500)
+
+# Taking under_2500 as interest variable 
+
+df_reg_under_2500<- readRDS(paste(path, "preprocessed_data.rds", sep = "/"))
+df_reg_under_2500$dbwt_inf_2500 <- ifelse(df_reg_under_2500$dbwt < 2500 , 1, 0)
+df_reg_under_2500 <- df_reg_under_2500%>% select(!c(dplural,dbwt,gest_under_37_weeks))
+
+reg_under_2500 <- lm(dbwt_inf_2500 ~ .,
+                     data = df_reg_under_2500)
+
+summary(reg_under_2500)
